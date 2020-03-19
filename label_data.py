289028d9558f3b2c3924432a9d6546d3167a94c1
typedwinter <typedwinter@gmail.com>
2020-03-19 20:37:08 +0800

    Added multiprocessing to script

--- label_data.py
+++ label_data.py
@@ -1,14 +1,38 @@
 import langdetect
+from multiprocessing import Pool
 
-with open('data/dota2_chat_messages.csv', 'r', encoding="utf8") as file:
-    with open('data/dota2_en_chat_messages.csv', 'a', encoding="utf8") as output_file:
+def filterEnglish(i):
+    with open('data/dota2_chat_messages.csv', 'r', encoding="utf8") as file:
+        arr = []
         for index, line in enumerate(file):
-            if index < 1243000:
+            if index < i[0]:
                 continue
+            if index == i[1]:
+                break
             if index % 1000 == 0:
                 print(index)
             try:
                 if langdetect.detect(line) == 'en':
-                    output_file.write(line)
+                    arr.append(line)
             except Exception:
                 pass
+        return arr
+
+def calc(start, end, np):
+    split = (end - start) // np
+    arr = []
+    for i in range(np):
+        arr.append([start+split*i, start+split*(i+1)])
+    return arr
+
+if __name__ == '__main__':
+    # set these variables
+    start = 50000
+    end = 60000
+    np = 4
+    with Pool(processes=np) as pool:
+        data = pool.map(filterEnglish, calc(start, end, np))
+    with open('data/dota2_en_chat_messages.csv', 'a', encoding="utf8") as output_file:
+        for arr in data:
+            for line in arr:
+                output_file.write(line)
